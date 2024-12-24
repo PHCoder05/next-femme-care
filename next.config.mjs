4423dec3cb6a6a7fa000e194005a46239db9e816
Pankaj Hadole <Pankajhadole4@gmail.com>
2024-12-25 00:51:21 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 const nextConfig = {
-    output: 'export', // Keep this if you're using static exports
-    images: {
-      unoptimized: true, // Disable Next.js Image Optimization
-    },
+      productionBrowserSourceMaps: true,
   };
   
   export default nextConfig;
